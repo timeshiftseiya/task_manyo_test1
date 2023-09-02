77c4a0dfabb3b37ff63721444fb68f6cb618ddf7
keisuzuki <kei.nishimura0131@gmail.com>
2023-09-02 22:38:45 +0900

    manyo_task_sep_2nd_2nd

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-        @task = Task.create!(title: '登録表示テスト', content: '登録表示のテスト内容')
+        @task = FactoryBot.create(:task)
         visit task_path(@task)
         expect(page).to have_content '登録表示テスト'
         expect(page).to have_content '登録表示のテスト内容'
@@ -16,7 +16,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを登録
-        Task.create!(title: '書類作成', content: '企画書を作成する。')
+        FactoryBot.create(:second_task)
         # タスク一覧画面に遷移
         visit tasks_path
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
@@ -28,7 +28,7 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     describe '任意のタスク詳細画面に遷移した場合' do
       before(:each) do
-        @task = Task.create!(title: '詳細テスト', content: '詳細表示のテスト内容')
+        @task = FactoryBot.create(:third_task)
       end
   
       it 'そのタスクの内容が表示される' do
