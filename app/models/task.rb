006f334626ba2f477a010eb5591470f59d86b732
keisuzuki <kei.nishimura0131@gmail.com>
2023-09-05 23:02:10 +0900

    manyo_task_sep5_6th

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
     validates :title, presence: true
-    validates :content, presence: { message: "Content can’t be blank" }
+    validates :content, presence: true
 end
