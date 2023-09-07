f7372bf834018cc62c68b1ed665bc9a2c781b422
keisuzuki <kei.nishimura0131@gmail.com>
2023-09-07 23:20:40 +0900

    manyo_task_sep7_1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module CdpWebManyoTask
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets false
       g.helper false
