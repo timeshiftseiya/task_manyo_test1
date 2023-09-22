03f349a607725b4195bb14b7ead3be9c409d6c13
keisuzuki <kei.nishimura0131@gmail.com>
2023-09-22 14:28:10 +0900

    sep-22-2

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
     validates :priority, presence: true
     validates :status, presence: true
 
-    enum priority: { "高": 0, "中": 1, "低": 2 }
+    enum priority: { "低": 0, "中": 1, "高": 2 }
     enum status: { "未着手": 0, "着手中": 1, "完了": 2 }
 
     paginates_per 10
